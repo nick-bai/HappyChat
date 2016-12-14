da26e5c423814584aa7c18a4e637def60215733a
白云飞 <876337011@qq.com>
2016-12-14 14:38:16 +0800

    add version check
    add version check

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
  * author: NickBai
  * createTime: 2016/12/9 0009 下午 4:19
  */
+if (version_compare(phpversion(), "5.4.0", "lt")) {
+    exit('php version must greater than 5.4.0');
+}
 ob_implicit_flush();
 require_once('./core/SocketChat.php');
 
