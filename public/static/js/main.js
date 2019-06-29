d3bd0c6ef6f03912992fa8eb6b2cc81232cb3909
白云飞 <876337011@qq.com>
2019-06-29 22:04:57 +0800

    完善聊天室

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -40,7 +40,12 @@ socket.on('connect', function(){
 
     // 展示消息
     socket.on("new message", function (data) {
+        $('#ding')[0].play();
         showMessage(data);
+
+        if(document.hidden){
+            showNotice(data.avatar, '您有新消息', data.message);
+        }
     });
 
     // 断开连接
@@ -152,6 +157,47 @@ function showUserList (data) {
             view.innerHTML += html;
         });
     });
+}
+
+function showNotice(head, title, msg) {
+    var Notification = window.Notification || window.mozNotification || window.webkitNotification;
+    if (Notification) {
+        Notification.requestPermission(function (status) {
+            //status默认值'default'等同于拒绝 'denied' 意味着用户不想要通知 'granted' 意味着用户同意启用通知
+            if ("granted" != status) {
+                return;
+            } else {
+                var tag = "sds" + Math.random();
+                var notify = new Notification(
+                    title,
+                    {
+                        dir: 'auto',
+                        lang: 'zh-CN',
+                        tag: tag,//实例化的notification的id
+                        icon: '/' + head,//通知的缩略图,//icon 支持ico、png、jpg、jpeg格式
+                        body: msg //通知的具体内容
+                    }
+                );
+                notify.onclick = function () {
+                    //如果通知消息被点击,通知窗口将被激活
+                    window.focus();
+                },
+                    notify.onerror = function () {
+                        console.log("HTML5桌面消息出错！！！");
+                    };
+                notify.onshow = function () {
+                    setTimeout(function () {
+                        notify.close();
+                    }, 2000)
+                };
+                notify.onclose = function () {
+                    console.log("HTML5桌面消息关闭！！！");
+                };
+            }
+        });
+    } else {
+        console.log("您的浏览器不支持桌面消息");
+    }
 }
 
 // 格式化时间
